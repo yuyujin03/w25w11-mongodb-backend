f93816f6600c06e4a9f73b1a600c5033e215dd61
youjin <youjinyeo12@gmail.com>
2025-11-20 16:31:39 +0900

    MONGO!!!

---
+++ w25w11-mongodb-backend/src/main/kotlin/kr/ac/kumoh/s20220667/w25w11_mongodb_backend/W25w11MongodbBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220667.w25w11_mongodb_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W25w11MongodbBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W25w11MongodbBackendApplication>(*args)
+}
---
+++ w25w11-mongodb-backend/src/test/kotlin/kr/ac/kumoh/s20220667/w25w11_mongodb_backend/W25w11MongodbBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220667.w25w11_mongodb_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25w11MongodbBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
