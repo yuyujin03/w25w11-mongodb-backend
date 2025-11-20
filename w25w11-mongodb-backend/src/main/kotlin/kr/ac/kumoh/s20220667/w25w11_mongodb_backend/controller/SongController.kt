e96a2cac9e221957b6e55dde3a424b11c57ed9e8
youjin <youjinyeo12@gmail.com>
2025-11-20 18:23:24 +0900

    🩺CORS 에러 해결

--- w25w11-mongodb-backend/src/main/kotlin/kr/ac/kumoh/s20220667/w25w11_mongodb_backend/controller/SongController.kt
+++ w25w11-mongodb-backend/src/main/kotlin/kr/ac/kumoh/s20220667/w25w11_mongodb_backend/controller/SongController.kt
@@ -2,13 +2,11 @@ package kr.ac.kumoh.s20220667.w25w11_mongodb_backend.controller
 
 import kr.ac.kumoh.s20220667.w25w11_mongodb_backend.model.Song
 import kr.ac.kumoh.s20220667.w25w11_mongodb_backend.service.SongService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/songs")
+@CrossOrigin(origins = ["http://localhost:5173"])
 class SongController(private val service: SongService) {
     @GetMapping
     fun getAllSongs(): List<Song> = service.getAllSongs()
